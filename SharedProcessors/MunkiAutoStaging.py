852cef4c5aba712e2240030328ca0b941926fe27
Matthias Choules <choules@wycomco.de>
2022-03-15 13:10:05 +0100

    pre-commit-fixes for SharedProcessors

--- SharedProcessors/MunkiAutoStaging.py
+++ SharedProcessors/MunkiAutoStaging.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # Copyright 2022 wycomco GmbH
 #
@@ -16,7 +17,6 @@
 """See docstring for MunkiAutoStaging class"""
 
 from __future__ import absolute_import
-from cgi import test
 
 import os
 import plistlib
@@ -32,6 +32,40 @@ from autopkglib.munkirepolibs.MunkiLib import MunkiLib
 __all__ = ["MunkiAutoStaging"]
 
 
+def _find_matching_item(repo_library, name):
+    """Looks through all catalog for items matching the named one.
+    Returns a list of all matching items if found."""
+
+    pkgdb = repo_library.make_catalog_db()
+
+    if not name:
+        return pkgdb["items"]
+
+    matching_items = []
+
+    for item in pkgdb["items"]:
+        if item["name"] == name:
+            matching_items.append(item)
+
+    return matching_items
+
+
+def _fetch_repo_library(
+    munki_repo,
+    munki_repo_plugin,
+    munkilib_dir,
+    repo_subdirectory,
+    force_munki_lib,
+):
+
+    if munki_repo_plugin == "FileRepo" and not force_munki_lib:
+        return AutoPkgLib(munki_repo, repo_subdirectory)
+
+    return MunkiLib(
+        munki_repo, munki_repo_plugin, munkilib_dir, repo_subdirectory
+    )
+
+
 class MunkiAutoStaging(Processor):
     """This processor will automatically move all given Munki items from a testing
     catalog to a production catalog after a given amount of days."""
@@ -44,8 +78,9 @@ class MunkiAutoStaging(Processor):
         },
         "MUNKI_REPO_PLUGIN": {
             "description": (
-                "Munki repo plugin. Defaults to FileRepo. Munki must be installed and available "
-                " at MUNKILIB_DIR if a plugin other than FileRepo is specified."
+                "Munki repo plugin. Defaults to FileRepo. Munki must be "
+                "installed and available at MUNKILIB_DIR if a plugin other "
+                "than FileRepo is specified."
             ),
             "required": False,
             "default": "FileRepo",
@@ -59,13 +94,16 @@ class MunkiAutoStaging(Processor):
         },
         "MUNKILIB_DIR": {
             "description": (
-                "Directory path that contains munkilib. Defaults to /usr/local/munki"
+                "Directory path that contains munkilib. Defaults to "
+                "/usr/local/munki"
             ),
             "required": False,
             "default": "/usr/local/munki",
         },
         "MUNKI_PKGINFO_FILE_EXTENSION": {
-            "description": "Extension for output pkginfo files. Default is 'plist'.",
+            "description": (
+                "Extension for output pkginfo files. Default is 'plist'.",
+            ),
             "required": False,
             "default": "plist",
         },
@@ -85,21 +123,21 @@ class MunkiAutoStaging(Processor):
         },
         "MUNKI_STAGING_DAYS": {
             "description": (
-                "Amount of days to keep a new item in staging catalog. Defaults to 5"
+                "Amount of days to keep a new item in staging catalog. "
+                "Defaults to 5"
             ),
             "required": False,
             "default": 5,
         },
         "NAME": {
-            "description": (
-                "Name of the Munki item to be checked."
-            ),
+            "description": ("Name of the Munki item to be checked."),
             "required": True,
         },
         "force_munki_repo_lib": {
             "description": (
-                "When True, munki code libraries will be utilized when the FileRepo plugin is "
-                "used. Munki must be installed and available at MUNKILIB_DIR"
+                "When True, munki code libraries will be utilized when the "
+                "FileRepo plugin is used. Munki must be installed and "
+                "available at MUNKILIB_DIR"
             ),
             "required": False,
             "default": False,
@@ -112,29 +150,10 @@ class MunkiAutoStaging(Processor):
         },
     }
 
-    def _find_matching_item(self, repo_library, name):
-        """Looks through all catalog for items matching the named one.
-        Returns a list of all matching items if found."""
-        
-        pkgdb = repo_library.make_catalog_db()
-
-        if not name:
-            return pkgdb["items"]
-
-        matching_items = []
-        
-        for item in pkgdb["items"]:
-            if item["name"] == name:
-                matching_items.append(item)
-
-        return matching_items
-
     def _find_pkginfo_files_in_repo(self, pkginfo, file_extension="plist"):
         """Returns the full path to pkginfo file in the repo."""
-        
-        destination_path = os.path.join(
-            self.env["MUNKI_REPO"], "pkgsinfo"
-        )
+
+        destination_path = os.path.join(self.env["MUNKI_REPO"], "pkgsinfo")
 
         if not os.path.exists(destination_path):
             raise ProcessorError(
@@ -143,92 +162,90 @@ class MunkiAutoStaging(Processor):
 
         if len(file_extension) > 0:
             file_extension = "." + file_extension.strip(".")
-        
+
         pkginfo_basename = f"{pkginfo['name']}-{pkginfo['version'].strip()}"
-        
-        file_list = glob.iglob(os.path.join(destination_path, "**", f"{pkginfo_basename}*{file_extension}"), recursive=True)
+
+        file_list = glob.iglob(
+            os.path.join(
+                destination_path, "**", f"{pkginfo_basename}*{file_extension}"
+            ),
+            recursive=True,
+        )
 
         return file_list
 
     def _find_items_to_promote(self, repo_library):
-        """Find and returns all pkginfo files which may be promoted to production catalog"""
+        """Find and returns all pkginfo files which may be promoted to
+        production catalog"""
 
-        items = self._find_matching_item(repo_library, self.env["NAME"])
+        items = _find_matching_item(repo_library, self.env["NAME"])
 
         items_to_promote = []
 
         for item in items:
             if "catalogs" not in item:
                 continue
-            
+
             if self.env["MUNKI_TESTING_CATALOG"] not in item["catalogs"]:
                 continue
-            
-            files = self._find_pkginfo_files_in_repo(item, self.env["MUNKI_PKGINFO_FILE_EXTENSION"])
+
+            files = self._find_pkginfo_files_in_repo(
+                item, self.env["MUNKI_PKGINFO_FILE_EXTENSION"]
+            )
 
             for file in files:
-                f = open(file, 'rb')
-                pkginfo = plistlib.load(f)
-                f.close()
+                with open(file, "rb") as file_handler:
+                    pkginfo = plistlib.load(file_handler)
+                    file_handler.close()
 
-                if ("catalogs" not in pkginfo) or (self.env["MUNKI_TESTING_CATALOG"] not in pkginfo["catalogs"]):
+                if ("catalogs" not in pkginfo) or (
+                    self.env["MUNKI_TESTING_CATALOG"]
+                    not in pkginfo["catalogs"]
+                ):
                     continue
-            
-                if ("_metadata" not in pkginfo) or ("creation_date" not in pkginfo["_metadata"]):
+
+                if ("_metadata" not in pkginfo) or (
+                    "creation_date" not in pkginfo["_metadata"]
+                ):
                     continue
 
                 delta = datetime.now() - pkginfo["_metadata"]["creation_date"]
-                
+
                 if delta.days > int(self.env["MUNKI_STAGING_DAYS"]):
-                   self.output(f"Found item to promote at {file}")
-                   items_to_promote.append(file)
-        
+                    self.output(f"Found item to promote at {file}")
+                    items_to_promote.append(file)
+
         return items_to_promote
 
     def promote_items(self, repo_library):
-        """Promotes all pkginfo items matching the given criteria to production catalog"""
-        
+        """Promotes all pkginfo items matching the given criteria to
+        production catalog"""
+
         files = self._find_items_to_promote(repo_library)
 
         versions_promoted = []
 
         for file in files:
-            f = open(file, 'rb')
-            pkginfo = plistlib.load(f)
-            f.close()
+            with open(file, "rb") as file_handler:
+                pkginfo = plistlib.load(file_handler)
+                file_handler.close()
 
             pkginfo["catalogs"].remove(self.env["MUNKI_TESTING_CATALOG"])
             pkginfo["catalogs"].append(self.env["MUNKI_PRODUCTION_CATALOG"])
             pkginfo["_metadata"]["promoted_by"] = os.getlogin()
             pkginfo["_metadata"]["promotion_date"] = datetime.now()
 
-            versions_promoted.append(pkginfo['version'])
+            versions_promoted.append(pkginfo["version"])
 
-            f = open(file, 'wb')
-            plistlib.dump(pkginfo, f)
-            f.close()
+            with open(file, "wb") as file_handler:
+                plistlib.dump(pkginfo, file_handler)
+                file_handler.close()
 
         return versions_promoted
 
-
-    def _fetch_repo_library(
-        self,
-        munki_repo,
-        munki_repo_plugin,
-        munkilib_dir,
-        repo_subdirectory,
-        force_munki_lib,
-    ):
-        if munki_repo_plugin == "FileRepo" and not force_munki_lib:
-            return AutoPkgLib(munki_repo, repo_subdirectory)
-        else:
-            return MunkiLib(
-                munki_repo, munki_repo_plugin, munkilib_dir, repo_subdirectory
-            )
-
     def main(self):
         try:
-            library = self._fetch_repo_library(
+            library = _fetch_repo_library(
                 self.env["MUNKI_REPO"],
                 self.env["MUNKI_REPO_PLUGIN"],
                 self.env["MUNKILIB_DIR"],
@@ -239,7 +256,7 @@ class MunkiAutoStaging(Processor):
             self.output(f"Using repo lib: {library.__class__.__name__}")
             self.output(f'        plugin: {self.env["MUNKI_REPO_PLUGIN"]}')
             self.output(f'          repo: {self.env["MUNKI_REPO"]}')
-            
+
             # clear any pre-existing summary result
             if "munki_autostaging_summary_result" in self.env:
                 del self.env["munki_autostaging_summary_result"]
@@ -249,26 +266,29 @@ class MunkiAutoStaging(Processor):
 
             if len(versions_promoted) > 0:
                 self.env["munki_repo_changed"] = True
-                
+
                 self.env["munki_autostaging_summary_result"] = {
-                    "summary_text": f"The following new items were promoted to catalog \"{self.env['MUNKI_PRODUCTION_CATALOG']}\":",
+                    "summary_text": (
+                        "The following new items were promoted to catalog "
+                        f"\"{self.env['MUNKI_PRODUCTION_CATALOG']}\":"
+                    ),
                     "report_fields": [
                         "name",
                         "versions",
                     ],
                     "data": {
                         "name": self.env["NAME"],
-                        "versions": ", ".join(versions_promoted)
+                        "versions": ", ".join(versions_promoted),
                     },
                 }
 
             else:
                 if "munki_repo_changed" not in self.env:
                     self.env["munki_repo_changed"] = False
-            
+
         except Exception as err:
             # handle unexpected errors here
-            raise ProcessorError(err)
+            raise ProcessorError(err) from err
 
 
 if __name__ == "__main__":
